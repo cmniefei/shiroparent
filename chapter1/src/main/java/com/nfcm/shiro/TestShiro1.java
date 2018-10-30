bb5e16a0667e1a9306efa3a5472f8fd10f7baf94
aaron <532047337@qq.com>
2018-10-30 10:29:45 +0800

    第一次提交

---
+++ chapter1/src/main/java/com/nfcm/shiro/TestShiro1.java
@@ -0,0 +1,36 @@
+package com.nfcm.shiro;
+
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
+public class TestShiro1 {
+    public static void main(String[] args) {
+        Factory<SecurityManager> factory =new IniSecurityManagerFactory("classpath:shiro.ini");
+        //得到安全管理器
+        SecurityManager securityManager = factory.getInstance();
+
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        try {
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
+        subject.checkPermission("");
+        System.out.println(subject.isAuthenticated());
+        subject.logout();
+
+        System.out.println(subject.isAuthenticated());
+
+    }
+}
